5f2e6e72bbc336be1b8445d808b7b5cd7634ebc8
Nemanja Nedeljković <nemanjan00@gmail.com>
2017-11-05 19:59:56 +0100

    Bootstrap

---
+++ src/newCanvas.js
@@ -0,0 +1,15 @@
+const isBrowser = require("./helpers/isBrowser");
+
+if(!isBrowser){
+	const canvas = require("canvas");
+
+	const createCanvas = canvas.createCanvas;
+	const loadImage = canvas.loadImage;
+}
+
+module.exports = function(canvas){
+	const newCanvas = {
+		
+	}
+}
+
